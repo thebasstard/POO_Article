cd839a70010e3245057a5a8e08b7fab508727bd4
ludo <ludo@LUDO>
2018-01-14 18:26:38 +0100

    Ajoutez des fichiers projet.

---
+++ Exo_Poo_Article/Article.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Poo_Article
+{
+    class Article
+    {
+        private int reference;
+        private string designation;
+        private double prixHT;
+        private static double tauxTVA;            //Attribut statique
+
+        public int Reference
+        {
+            get { return reference; }
+            set { reference = value; }
+        }
+
+        public string Designation
+        {
+            get { return designation; }
+            set { designation = value; }
+        }
+
+        public double PrixHT
+        {
+            get { return prixHT; }
+            set { prixHT = value; }
+        }
+
+        public static double TauxTVA                             //propriété statique
+        {
+            get { return tauxTVA; }
+            set { tauxTVA = value; }
+        }
+
+        public Article() { }                                       //Constructeur par défaut
+
+
+        //permet de renseigner la référence et la désignation lors de l’instanciation
+        public Article(int r, string d)                             //Constructeur surchargé
+        {
+            reference = r;
+            designation = d;
+        }
+
+        public Article(Article a)                                  //Constructeur de recopie
+        {
+            reference = a.reference;
+            designation = a.designation;
+            prixHT = a.prixHT;
+
+        }
+
+        public Article(int r, string d, double p)         //Constructeur d'initialisation
+        {
+            reference = r;
+            designation = d;
+            prixHT = p;
+
+        }
+
+        public double CalculerPrixTTC()
+        {
+            return prixHT * (1 + tauxTVA / 100);
+        }
+
+        public void AfficherArticle()
+        {
+            Console.Out.WriteLine("Référence : " + reference);
+            Console.Out.WriteLine("Désignation : " + designation);
+            Console.Out.WriteLine("Prix HT : " + prixHT);
+        }
+        
+    }
+}
---
+++ Exo_Poo_Article/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Poo_Article
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Out.Write("Donner le taux de TVA pour tous les articles : ");
+            Article.TauxTVA = double.Parse(Console.In.ReadLine()); //propriété statique
+            Console.Out.WriteLine("Le taux TVA est : " + Article.TauxTVA + "%");
+
+            Console.Out.WriteLine("\n\nArticle 1: ");
+            Article A1 = new Article();
+            A1.AfficherArticle();
+            Console.Out.WriteLine("Le prix TTC est " + A1.CalculerPrixTTC());
+
+            Console.Out.WriteLine("\n\nArticle 2: ");
+            int r;
+            string d;
+            double p;
+            Console.Out.Write("Donner la référence: ");
+            r = int.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner la désignation: ");
+            d = Console.In.ReadLine();
+            Console.Out.Write("Donner le prix HT: ");
+            p = double.Parse(Console.In.ReadLine());
+            Article A2 = new Article(r, d, p);
+            A2.AfficherArticle();
+            Console.Out.WriteLine("Le prix TTC est " + A2.CalculerPrixTTC());
+
+            Console.Out.WriteLine("\n\nArticle 3: ");
+            Console.Out.Write("Donner la référence: ");
+            r = int.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner la désignation: ");
+            d = Console.In.ReadLine();
+            Article A3 = new Article(r, d);
+            A3.AfficherArticle();
+            Console.Out.WriteLine("Le prix TTC est " + A3.CalculerPrixTTC());
+
+            Console.Out.WriteLine("\n\nArticle 4: ");
+            Article A4 = new Article(A2);
+            A4.AfficherArticle();
+            Console.Out.WriteLine("Le prix TTC est " + A4.CalculerPrixTTC());
+
+            Console.ReadKey();
+
+        }
+    }
+}
